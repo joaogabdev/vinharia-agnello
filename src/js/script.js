38fa92eb9dc8ee73415f1a5f949e33f7047de5d6
Lucas Hideo <lucashideo2007@gmail.com>
2026-09-23 20:39:21 -0300

    feat: Conexão do botão feito no HTML para uma função no css

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+var botaoCadastrar = document.getElementById("btn-cadastrarvinhos");
+
+var botaoCadastrar = document.getElementById("btn-cadastrarvinhos");
+
+botaoCadastrar.addEventListener("click", cadastrarVinho);
