373b965049d186234b9d840eb8e480398951fb56
kethelenj <ketyjacobina@gmail.com>
2026-09-23 21:02:21 -0300

    feat: Exclusão da primeira linha duplicada.

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 var botaoCadastrar = document.getElementById("btn-cadastrarvinhos");
 
-var botaoCadastrar = document.getElementById("btn-cadastrarvinhos");
-
 botaoCadastrar.addEventListener("click", cadastrarVinho);
 
 function cadastrarVinho() {
